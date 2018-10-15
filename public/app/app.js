92e5bd00bcbcfbb167fd30058bb5c3ba1725112c
HariprasadNuttu <hariprasadnuttu@gmail.com>
2018-10-15 08:26:41 +0530

    Hariprasad-added forgot and reset passwod views

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,9 @@ angular.module('HopeResearchApp',['ngResource', 'ngRoute']).config(function($rou
     $locationProvider.html5Mode(true);
     $routeProvider
         .when('/sign_up', { templateUrl: '/views/sign_up.html', controller: 'SignUpController'})
-        .when('/sign_in', { templateUrl: '/views/sign_in.html', controller: 'SignUpController'});
+        .when('/sign_in', { templateUrl: '/views/sign_in.html', controller: 'SignUpController'})
+        .when('/forgot_password', { templateUrl: '/views/forgot_password.html', controller: 'SignUpController'})
+          .when('/reset_password', { templateUrl: '/views/reset_password.html', controller: 'SignUpController'})
         $routeProvider.otherwise('/sign_in');
 })
 .controller('SignUpController', function ($scope,authenticationFactory) {
@@ -11,15 +13,19 @@ angular.module('HopeResearchApp',['ngResource', 'ngRoute']).config(function($rou
   $scope.form={};
   // $scope.rexp_email="/^(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\s*[,]?\b)*$/";
   $scope.signIn=function(){
-    console.log($scope.signinForm)
-    // if($scop.signinForm)
     authenticationFactory.signIn($scope.signin_credentials);
   }
   $scope.signUp=function(){
-    console.log($scope.signinForm)
-    // if($scop.signinForm)
     authenticationFactory.signUp($scope.signup_credentials);
   }
+  $scope.forgotPassword=function(){
+    authenticationFactory.forgotPassword($scope.signup_credentials);
+  }
+  $scope.resetPassword=function(){
+    authenticationFactory.resetPassword($scope.signup_credentials);
+  }
+
+
   function setFormTouched(form_obj){
   angular.forEach(form_obj.$error, function (field) {
     angular.forEach(field, function (errorField) {
@@ -54,6 +60,36 @@ angular.module('HopeResearchApp',['ngResource', 'ngRoute']).config(function($rou
       console.log("HariprasadNuttu")
       var defferd=$q.defer();
       $http.post("users/sign_up",params)
+      .success(function(data){
+        if(data.success){
+          defferd.resolve(data);
+        }else{
+          defferd.reject(data);
+        }
+      }).catch(function(error){
+        defferd.reject(error);
+      })
+      return defferd.promise;
+    },
+    forgotPassword:function(params){
+      console.log("HariprasadNuttu")
+      var defferd=$q.defer();
+      $http.post("users/forgot_password",params)
+      .success(function(data){
+        if(data.success){
+          defferd.resolve(data);
+        }else{
+          defferd.reject(data);
+        }
+      }).catch(function(error){
+        defferd.reject(error);
+      })
+      return defferd.promise;
+    },
+    resetPassword:function(params){
+      console.log("HariprasadNuttu")
+      var defferd=$q.defer();
+      $http.post("users/reset_password",params)
       .success(function(data){
         if(data.success){
           defferd.resolve(data);
